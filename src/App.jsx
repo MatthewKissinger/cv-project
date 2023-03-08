1cc2eb546f29338dbfd50896d2c9801c1d23219c
Matt <matthew.t.kissinger@gmail.com>
2023-03-08 12:02:54 -0800

    state object created in App.jsx class component

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,46 @@ import Header from './components/Header'
 import GeneralInfo from './components/GeneralInfo'
 import './styles/app.css'
 
+// create a state object for all resume items
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: 'Joe Schmoe',
+      title: 'Web Developer',
+      phone: '555.867.5309',
+      email: 'joe.schmoe@gmail.com',
+      addressStreet: '1234 Sesame St.',
+      addressCity: 'New York City, NY 10001'
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    console.log(e.target)
+  }
   
   render() {
     return (
       <div className="App">
         <Header />
         <div className='resume--container'>
-          <GeneralInfo />
+          <GeneralInfo 
+            toggleClick={this.handleClick}
+            userName={this.state.name}
+            userTitle={this.state.title}
+            userPhone={this.state.phone}
+            userEmail={this.state.email}
+            userStreet={this.state.addressStreet}
+            userCity={this.state.addressCity}
+          />
         </div>
       </div>
     )
   }
 }
 
-export default App
+export default App;
--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -2,27 +2,35 @@ import React from 'react';
 import '../styles/generalInfo.css'
 
 class GeneralInfo extends React.Component {
+    
     render() {
+        
+        const { userName, userTitle, userPhone, userEmail, userStreet, userCity, toggleClick } = this.props;
+        
         return (
             <div className='general--container'>
                 <div className='general--left'>
-                    <h1 className='general--name input'>Matt Kissinger</h1>
-                    <h3 className='general--title input'>Software Developer</h3>
+                    <h1 
+                        onClick={toggleClick}
+                        className='general--name input'>
+                        {userName}
+                    </h1>
+                    <h3 className='general--title input'>{userTitle}</h3>
                 </div>
                 <div className='general--right'>
                     <div className='general--phone input'>
                         <i className='material-icons'>phone</i>
-                        <p>773.698.4956</p>
+                        <p>{userPhone}</p>
                     </div>
                     <div className='general--email input'>
                         <i className='material-icons'>mail</i>
-                        <p>matthew.t.kissinger@gmail.com</p>
+                        <p>{userEmail}</p>
                     </div>
                     <div className='general--address input'>
                         <i className='material-icons'>place</i>
                         <div>
-                            <p>10506 Riverside Dr.</p>
-                            <p>Toluca Lake, CA 91602</p>
+                            <p>{userStreet}</p>
+                            <p>{userCity}</p>
                         </div>
                     </div>
                 </div>
