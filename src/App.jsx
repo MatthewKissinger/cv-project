34b79569c73276fdad5fcdebe40b9eb8b92e2ca6
Matt <matthew.t.kissinger@gmail.com>
2023-03-07 19:41:53 -0800

    added and styled Header.jsx class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import Header from './components/Header'
+import './styles/app.css'
 
 function App() {
 
   return (
     <div className="App">
-      Hello World
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/header.css'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <h1>Resume Builder</h1>
+            </header>
+        )
+    }
+}
+
+export default Header
